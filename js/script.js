0a3e5befdae9a00f7dbcb2c4dc439a4d23455c9c
Phoebe Vanthournout <phoebe.vanthournout@student.howest.be>
2025-06-20 10:33:21 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// const container = document.querySelector('.buttons');
+// const buttons = container.querySelectorAll('button');
+
+// container.addEventListener('mouseover', (e) => {
+//   if (e.target.tagName === 'BUTTON') {
+//     buttons.forEach(btn => {
+//       btn.style.opacity = (btn === e.target) ? '1' : '0.4';
+//     });
+//   }
+// });
+
+// container.addEventListener('mouseout', (e) => {
+//   // check of je de container verlaat en niet gewoon naar een andere knop gaat
+//   if (!container.contains(e.relatedTarget)) {
+//     buttons.forEach(btn => {
+//       btn.style.opacity = '1';
+//     });
+//   }
+// });
